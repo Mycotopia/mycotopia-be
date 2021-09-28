923fa73d385945362c9b1227071eca936f45e4c4
PrinceShaji <22243569+PrinceShaji@users.noreply.github.com>
2021-09-28 20:35:14 +0530

    Created SignUp route (without request validation or password hashing)

--- controllers/signUp.js
+++ controllers/signUp.js
@@ -4,7 +4,37 @@ const { prisma } = require("../services/db");
 module.exports = exports = {};
 
 exports.signUp = async (req, res) => {
-    let { } = req.body;
-    let user = await prisma.user.findMany();
-    res.status(200).json([...user]);
+    let { fullName, username, emailAddress, password } = req.body;
+
+    // Repacking user name.
+    const firstName = fullName.split(' ')[0];
+    const lastName = fullName.split(' ').slice(1).join(' ');
+    // Request IP address
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    // Request User-Agent.
+    const userAgent = req.headers['user-agent'] || 'none'
+
+    // Only get email address. Query contains sensitive data.
+    const { email } = await prisma.user.create({
+        data: {
+            firstName,
+            lastName,
+            email: emailAddress,
+            userId: username,
+            password,
+            profile: {
+                create: {}
+            },
+            signUpMetadata: {
+                create: {
+                    ipAddress: ip,
+                    userAgent: userAgent
+                }
+            }
+        }
+    }).catch(err => {
+        console.error(err);
+        return res.status(409).json({ "message": "Duplicate username or email." })
+    })
+    return res.status(201).json({ email });
 }
--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
+const { signUp } = require("./controllers/signUp");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello Mycotopia.')
+    res.send("Mycotopia Home Page.");
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.info(`Server running on  http://localhost:${process.env.PORT || 3000}`)
+app.post("/signup/", (req, res) => {
+    signUp(req, res);
+})
+
+app.listen(process.env.PORT || 3001, () => {
+    console.info(`Server running on  http://localhost:${process.env.PORT || 3001}`)
 })
