7f9ef94aff4ae80f7888890b0f2d8e86554ae53f
PrinceShaji <22243569+PrinceShaji@users.noreply.github.com>
2021-10-03 20:00:57 +0530

    Fixed login bug #17

--- controllers/logIn.js
+++ controllers/logIn.js
@@ -8,7 +8,6 @@ module.exports = exports = {};
 exports.logIn = async (req, res) => {
 
     let session = req.session;
-    console.log(session)
     if (session.user_id) {
         return res.status(250).json({ "message": "user already logged in" });
     } else {
@@ -20,34 +19,42 @@ exports.logIn = async (req, res) => {
         // Request User-Agent.
         const userAgent = req.headers['user-agent'] || 'none'
 
-
-        const { id, "password": dbPasswordHash } = await prisma.user.findUnique({
+        let user = await prisma.user.findUnique({
             where: {
                 user_id: username
             }
-        }).catch(err => { console.error(err); return res.status(500); });
+        }).then(async user => {
+            if (user === null) {
+                throw new Error("user does not exist");
+            }
+
+            const { id, "password": dbPasswordHash } = user;
+
+            if (auth.comparePassword(userPassword, dbPasswordHash) === true) {
+
+                const { "id": userSessionsId } = await prisma.userSessions.create({
+                    data: {
+                        token: req.session.id,
+                        ip_address: ip_address,
+                        user_agent: userAgent,
+                        user_id: id
+                    }
+                })
 
 
-        if (auth.comparePassword(userPassword, dbPasswordHash) === true) {
-
-            const { "id": userSessionsId } = await prisma.userSessions.create({
-                data: {
-                    token: req.session.id,
-                    ip_address: ip_address,
-                    user_agent: userAgent,
-                    user_id: id
-                }
-            })
+                // Setting session with the user id.
+                req.session.user_id = id;
+                req.session.user_session_id = userSessionsId;
 
 
-            // Setting session with the user id.
-            req.session.user_id = id;
-            req.session.user_session_id = userSessionsId;
-
-
-            return res.status(200).json({ "message": "success" });
-        } else {
-            return res.status(401).json({ "message": "Wrong username or password." })
-        }
+                return res.status(200).json({ "message": "success" });
+            } else {
+                return res.status(401).json({ "message": "Wrong username or password." })
+            }
+        }).catch(err => {
+            console.error(err);
+            res.status(404).json({ "message": "User does not exist." });
+            return
+        });
     }
 }
