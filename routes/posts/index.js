ffddd68ac3d86d7fe2c7ff1c04855d2b0fb35043
PrinceShaji <22243569+PrinceShaji@users.noreply.github.com>
2021-10-29 18:17:45 +0530

    Added delete post feature

--- routes/posts/index.js
+++ routes/posts/index.js
@@ -1,12 +1,16 @@
 'use-strict'
 
 import express from "express";
-import { createPost } from "../../controllers/posts/index.js";
+import { createPost, deletePost } from "../../controllers/posts/index.js";
 
 const postsRouter = express.Router();
 
 postsRouter.post("/create", (req, res) => {
     createPost(req, res);
+});
+
+postsRouter.delete("/:post_id", (req, res) => {
+    deletePost(req, res);
 });
 
 export default postsRouter;
